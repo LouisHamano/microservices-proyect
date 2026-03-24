1bb8996fa31a0690a4f8c30c1d633405c60b4b28
Lgarcia <lgarcia@topstrategic.net>
2026-03-24 04:03:26 -0500

    add user-microservice

--- user-microservice/src/main/java/com/corporation_dev/user_microservice/controller/UserController.java
+++ user-microservice/src/main/java/com/corporation_dev/user_microservice/controller/UserController.java
@@ -6,11 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.corporation_dev.user_microservice.dto.UserDto;
 import com.corporation_dev.user_microservice.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+
 @RestController
-@RequestMapping("/user")
+@RequestMapping("user")
+@Tag(name = "User API", description = "Endpoints for managing users")
 public class UserController {
     private UserService userService;
     
@@ -18,13 +22,20 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Get all users", description = "Returns a list of all users in the system")
     @GetMapping("all")
     public Flux<UserDto> getAllUsers() {
         return this.userService.getAllUsers();
     }
 
+    @Operation(summary = "Insert a new user", description = "Inserts a new user into the system")
     @PostMapping("insert")
     public Mono<UserDto> insertUser(@RequestBody Mono<UserDto> userDto) {
         return this.userService.insertUser(userDto);
+    } 
+
+    @GetMapping("test")
+    public Mono<String> testCircuit() {
+        return userService.testCircuitBreaker();
     }
 }
--- user-microservice/src/main/java/com/corporation_dev/user_microservice/service/UserService.java
+++ user-microservice/src/main/java/com/corporation_dev/user_microservice/service/UserService.java
@@ -3,17 +3,19 @@ import org.springframework.stereotype.Service;
 import com.corporation_dev.user_microservice.dto.UserDto;
 import com.corporation_dev.user_microservice.repository.UserRepository;
 import com.corporation_dev.user_microservice.util.EntityDtoUtil;
-
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
 public class UserService { 
+    private UserRepository userRepository;    
+    private WebClient webClient;
 
-    private UserRepository userRepository;
-
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, WebClient webClient) {
         this.userRepository = userRepository;
+        this.webClient = webClient;
     }
 
     public Flux<UserDto> getAllUsers() {
@@ -21,10 +23,51 @@ public class UserService {
             .map(EntityDtoUtil::toDto);
     }
 
+    @CircuitBreaker(name = "insertUserCircuitBreaker", fallbackMethod = "insertUserFallback")
     public Mono<UserDto> insertUser(Mono<UserDto> userDto) {
         return userDto.map(EntityDtoUtil::toEntity)
             .flatMap(userRepository::save)
             .map(EntityDtoUtil::toDto);
     }
 
+    public Mono<UserDto> insertUserFallback(Mono<UserDto> userDto, Throwable ex) {
+        System.out.println("Fallback insertUser - Error: " + ex.getMessage());
+        return Mono.error(new RuntimeException("Servicio de usuarios no disponible temporalmente"));
+    }
+
+    @CircuitBreaker(name = "testCircuitBreaker", fallbackMethod = "testCircuitBreakerFallback")
+    public Mono<String> testCircuitBreaker() {
+        return webClient
+            .get()
+            .uri("http://www.googleerror.com")
+            .retrieve()
+            .bodyToMono(String.class)
+            .map(res -> "Operación exitosa")
+            .doOnError(ex -> System.out.println("-> Error capturado: " + ex.getClass().getSimpleName()))
+            .onErrorMap(ex -> new RuntimeException("Servicio de prueba no disponible"));
+    }
+
+    public Mono<String> testCircuitBreakerFallback(Throwable ex) {
+        System.out.println("-> FALLBACK ejecutado: Servicio temporalmente no disponible");
+        return Mono.just("Fallback: Servicio no disponible");
+    }
+
+    /* Falta implementar */
+    // Actualizar un usuario existente
+    // public Mono<User> updateUser(int id, User userDetails) {
+    //     return userRepository.findById(id)
+    //         .flatMap(existingUser -> {
+    //             existingUser.setName(userDetails.getName());
+    //             existingUser.setEmail(userDetails.getEmail());
+    //             existingUser.setPassword(userDetails.getPassword());
+    //             existingUser.setRole(userDetails.getRole());
+    //             existingUser.setStatus(userDetails.getStatus());
+    //             return userRepository.save(existingUser);
+    //         });
+    // }
+
+    // // Eliminar un usuario
+    // public Mono<Void> deleteUser(int id) {
+    //     return userRepository.deleteById(id);
+    // }
 }
