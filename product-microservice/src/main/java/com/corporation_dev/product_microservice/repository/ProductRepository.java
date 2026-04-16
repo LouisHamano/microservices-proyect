0edbd5611487e592e65cfcd3739de5f81cefdb5f
Lgarcia <lgarcia@topstrategic.net>
2026-04-16 02:58:07 -0500

    product-microservice: reset → delete mongodb

--- product-microservice/src/main/java/com/corporation_dev/product_microservice/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.corporation_dev.product_microservice.repository;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
-import com.corporation_dev.product_microservice.entity.Product;
-
-@Repository
-public interface ProductRepository extends ReactiveMongoRepository<Product, String>{
-}
